0b7ee653a821bac978f22d71eaea39714d6843c7
666-CATAHA <murad2013minecraft@inbox.ru>
2026-03-15 10:37:44 +0500

    Delete sait dla proverki directory

--- sait dla proverki/functions/auth/logout.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet() {
-  const headers = new Headers();
-  headers.append(
-    "Set-Cookie",
-    "session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0"
-  );
-  headers.set("Location", "/");
-  return new Response(null, { status: 302, headers });
-}
